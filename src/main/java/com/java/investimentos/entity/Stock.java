bb06fa31cf6dc4182f522ec5aab297d5d48ea19e
Davisond <davisonazevedo05@gmail.com>
2026-04-17 20:19:32 -0300

    criado estidade stock, nao foi utilizado geracao de UUID pq o ID vai vir do codigo da acao, como PETR4 por ex

---
+++ src/main/java/com/java/investimentos/entity/Stock.java
@@ -0,0 +1,41 @@
+package com.java.investimentos.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_stocks")
+public class Stock {
+
+    @Id
+    @Column(name = "stock_id")
+    private String stockId; //PETR4, VALE etc
+
+    @Column(name = "description")
+    private String description;
+
+    public Stock() {
+    }
+
+    public Stock(String description, String stockId) {
+        this.description = description;
+        this.stockId = stockId;
+    }
+
+    public String getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
